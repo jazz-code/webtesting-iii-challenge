3c854790baecf26334320290edc5217c42774ec2
ant <ren.meta@gmail.com>
2019-08-19 18:22:40 -0400

    Display.spec.js finished

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -16,4 +16,18 @@ describe("<Controls />", () => {
         wrapper = rtl.render(<Display closed={true} locked={false} />);
         expect(wrapper.container.firstChild.firstChild.textContent).toBe("Unlocked");
     })
+
+    it("displays 'Closed' if the closed prop is true and 'Open' if otherwise", () => {
+        let wrapper = rtl.render(<Display closed={true} locked={true} />);
+        expect(wrapper.container.firstChild.lastChild.textContent).toBe("Closed");
+      
+        wrapper = rtl.render(<Display closed={false} locked={false} />);
+        expect(wrapper.container.firstChild.lastChild.textContent).toBe("Open");
+    })
+
+    it("when locked or closed use the red-led class", () => {
+        const wrapper = rtl.render(<Display closed={true} locked={true} />);
+        expect(wrapper.container.firstChild.firstChild.classList[1]).toBe("red-led")
+        expect(wrapper.container.firstChild.lastChild.classList[1]).toBe("red-led")
+    })
 })
