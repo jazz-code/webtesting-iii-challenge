7b7d637ea5573f5d778dd11743bb969d1d75395f
ant <ren.meta@gmail.com>
2019-08-19 17:00:50 -0400

    shows the controls and the display

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -13,6 +13,17 @@ describe("<Controls />", () => {
         expect(closebtn).toBeDefined();
         expect(lockbtn).toBeDefined();
     })
-    
-    
+
+    it("cannot be closed or opened if it is locked",async () =>
+{
+        let wrapper = rtl.render(<Controls locked={true} closed={true}/>);
+        let button = wrapper.getByTestId('toggle-closed');
+        await rtl.fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+        rtl.cleanup();
+        wrapper = rtl.render(<Controls locked={false} closed={false}/>);
+        button = wrapper.getByTestId('toggle-locked');
+        await rtl.fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+    })
 })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import Dashboard from "./Dashboard";
+
+it('shows the controls and display', () => {
+  const wrapper = rtl.render(<Dashboard />);
+});
+
