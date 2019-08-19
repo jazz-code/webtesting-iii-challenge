234211cea72e8711fe4b1f9ebf429fd80acbe965
ant <ren.meta@gmail.com>
2019-08-19 16:37:12 -0400

    added data-testid and tested for default buttons, passed tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent,} from '@testing-library/react'
+import * as rtl from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+    it("defaults to unlocked and open", async () => {
+        const wrapper = rtl.render(<Controls locked={false} closed={false}/>);
+        let closebtn = wrapper.getByTestId('toggle-closed');
+        let lockbtn = wrapper.getByTestId('toggle-locked');
+        expect(closebtn).toBeDefined();
+        expect(lockbtn).toBeDefined();
+    })
+    
+    
+})
