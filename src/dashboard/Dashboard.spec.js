69183d28ecef92ddc78c17621e727b20a63874b8
ant <ren.meta@gmail.com>
2019-08-19 17:19:22 -0400

    displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,7 +2,10 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import Dashboard from "./Dashboard";
 
-it('shows the controls and display', () => {
-  const wrapper = rtl.render(<Dashboard />);
-});
+
+describe("<Controls />", () => {
+    it('shows the controls and display', () => {
+    const wrapper = rtl.render(<Dashboard />);
+    });
+})
 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+
+import React from 'react';
+import Display from "./Display";
+import * as rtl from '@testing-library/react';
+
+
+describe("<Controls />", () => {
+    it('shows the display', () => {
+        const wrapper = rtl.render(<Display />);
+    });
+
+    it("displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise", () => {
+        let wrapper = rtl.render(<Display closed={true} locked={true} />);
+        expect(wrapper.container.firstChild.firstChild.textContent).toBe("Locked");
+
+        wrapper = rtl.render(<Display closed={true} locked={false} />);
+        expect(wrapper.container.firstChild.firstChild.textContent).toBe("Unlocked");
+    })
+})
